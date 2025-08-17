f58c2ff5cd88b4007f72cf379819137db963a2d7
sneha-yadav786 <sneha.7864786@gmail.com>
2025-08-17 23:55:31 +0530

    Updated prob-2264.py

--- prob-2264.py
+++ prob-2264.py
@@ -1,15 +1,5 @@
-class Solution:
-    def largestGoodInteger(self, num: str) -> str:
-        l=[]
-        for i in range(len(num)):
-            if len(set(num[i:i+3]))==1 and len(num[i:i+3])==3:
-                l.append(num[i:i+3])
-        if not l:
-            return ""
-        else:
-            maxi=l[0]
-            for j in range(1,len(l)):
-                if int(l[j])>=int(maxi):
-                    maxi=l[j]
-            
-            return maxi
+ans=0
+s=[2,3,4,5]
+for i in s:
+    ans^=i
+print(ans)
