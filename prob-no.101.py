347ebf444d2bbbb1594f7becd70246b5ebbb5440
sneha-yadav786 <sneha.7864786@gmail.com>
2026-01-28 23:44:53 +0530

    Added solution no. 101

---
+++ prob-no.101.py
@@ -0,0 +1,10 @@
+class Solution:
+    def check(self,n1,n2):
+        if not n1 and not n2:
+            return True
+        if not n1 or not n2:
+            return False
+        return n1.val==n2.val and self.check(n1.left,n2.right) and self.check(n1.right,n2.left)
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.check(root.left,root.right)
+        
