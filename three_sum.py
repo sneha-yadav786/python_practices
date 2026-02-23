7846fcc5780acd717bc4af75d17bf5b7b3802e58
sneha-yadav786 <sneha.7864786@gmail.com>
2026-02-23 21:58:16 +0530

    Updated three sum solution

--- three_sum.py
+++ three_sum.py
@@ -1,32 +1,37 @@
+from collections import Counter
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums=sorted(nums)
-        a=[]
-        i,j=0,len(nums)-1
-        while i<j:
-          d=nums[i+1:j]
-          b=[]
-          k=0
-          while k<len(d):
+    def threeSum(self, nums):
+        nums.sort()
+        res = []
+        n = len(nums)
 
-            if d[k] not in b:
-              if nums[i]+nums[j] <-d[k]:
-               k+=1
-               
-              elif nums[i]+nums[j] >-d[k]:
-               break
-               
-              else:
-               l=[]
-               l.append(nums[i])
-               l.append(nums[j])
-               l.append(d[k])
-               a.append(l)
-               b.append(k)
-               j-=1
-               i=0
-            else:
-              i+=1
-        return (a)
-s1=Solution()
-print(s1.threeSum([-1,-1,1,2,-2,0,4 ]))
+        for i in range(n - 2):
+
+            # skip duplicate fixed element
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left, right = i + 1, n - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    res.append([nums[i], nums[left], nums[right]])
+
+                    # skip duplicates
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]: #YHI STEP HMARE DUPLICATES KO REMOVE KRTE HAI VRNA O(N) HAR BAAR LAGTA CHECK KRNE ME {IF ITEM IN LIST...}
+                        right -= 1
+
+                    left += 1
+                    right -= 1
+
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return res
+# BUT AISA APPROACH HUM TWO SUM VALE ME NHI USE KRENGE KYUKI YE O(N^2) HAI AUR TWO SUM KA OPTIMIZED SOLUTION O(N) ME SOLVE HOTA HAI
